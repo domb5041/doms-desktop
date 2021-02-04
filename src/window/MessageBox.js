435e599c70ee846b6526e8936e151270ceae9e65
domb5041 <domb5041@icloud.com>
2021-02-04 12:59:05 +0000

    fixed safari messagebox inset

--- src/window/MessageBox.js
+++ src/window/MessageBox.js
@@ -62,7 +62,10 @@ const StyledMessageBox = styled.div`
 
 const StyledShade = styled.div`
     position: fixed;
-    inset: 0 0 0 0;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
     background-color: rgba(0, 0, 0, 0.2);
     z-index: 8000;
     &.shade-enter {
